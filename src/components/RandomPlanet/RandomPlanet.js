4702b81549340b4df6242f5d52867df36c4dcc16
Viola <violetta.artiukhova@skey.team>
2024-04-06 10:07:12 +0200

    new function

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -7,15 +7,17 @@ export default class RandomPlanet extends Component {
     swapiService = new SwapiService()
     state = {
         planet: {},
-        loading: false,
-        error: false,
+        loading: true,
     }
 
-    constructor() {
-        super()
+    componentDidMount() {
         this.updatePlanet()
+        this.interval = setInterval(this.updatePlanet, 5000)
     }
 
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
     onPlanetLoaded = (planet) => {
         this.setState({ planet, loading: false, error: false })
     }
@@ -26,14 +28,16 @@ export default class RandomPlanet extends Component {
             loading: false,
         })
     }
-    updatePlanet() {
-        const id = 15
+    updatePlanet = () => {
+        console.log('update')
+        const id = Math.floor(Math.random() * 25) + 3
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
             .catch(this.onError)
     }
     render() {
+        console.log('render')
         const { planet, loading, error } = this.state
 
         const hasData = !(loading || error)
